451ffb26aae29a523ac91626d5fa0d6d027011f4
joakim engberg <joakim.engberg@gmail.com>
2017-12-01 21:18:07 +0100

    Initial commit

---
+++ TV-bank.ino
@@ -0,0 +1,41 @@
+#include <dht.h>
+
+dht DHT;
+
+#define DHT11_PIN 3
+int fan = 2;
+
+
+void setup(){
+  Serial.begin(9600);
+  pinMode(fan, OUTPUT);
+  digitalWrite(fan, HIGH);
+}
+
+void loop()
+{
+  int chk = DHT.read11(DHT11_PIN);
+  int temp=DHT.temperature;
+  Serial.print("Temperature = ");
+  Serial.println(DHT.temperature);
+  
+if (temp >= 23)
+{
+  Serial.println("Temperatur hög - Fläkt på  ");
+  digitalWrite(fan, LOW);
+  delay(600);
+}
+else if(temp <= 20)
+{
+  Serial.println("Temperatur ok - Av");
+  digitalWrite(fan, HIGH);
+  }
+  else
+{
+  Serial.println("Temperatur ok  ");
+//  digitalWrite(fan, HIGH);
+  delay(600);
+}
+delay(3000);
+}
+
